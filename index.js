c85d90312edd0fdad0fdc4cf94edc555c2309bea
冯程豪 <fengchenghao@xiaoyouzi.com>
2018-11-06 17:06:37 +0800

    卢必新和王建荣报名

--- index.js
+++ index.js
@@ -13,24 +13,33 @@ function App() {
             <Timeline pending="期待更多...">
                 <Timeline.Item color="green">
                     <p>2018-09-24</p>
-                    <p>聚石塔日志的收集</p>
+                    <p>聚石塔日志的收集与检索</p>
                     <p>@余新乐</p>
                 </Timeline.Item>
                 <Timeline.Item color="green">
                     <p>2018-09-26</p>
-                    <p>聚石塔</p>
+                    <p>电商业务架构</p>
                     <p>@胡小东</p>
                 </Timeline.Item>
                 <Timeline.Item color="green">
-                    <p>@胡小东</p>
                     <p>2018-10-02</p>
-                    <p>聚石塔日志</p>
+                    <p>日志规范及目录说明</p>
                     <p>@张艳恒</p>
                 </Timeline.Item>
                 <Timeline.Item dot={<Icon type="clock-circle-o" style={{fontSize: '16px'}}/>}>
-                    <p>时间待定</p>
-                    <p>主题正在征集中</p>
+                    <p>2018-10-14</p>
+                    <p>探索String、StringBuffer、StringBuilder</p>
+                    <p>@卢必新</p>
                 </Timeline.Item>
+                <Timeline.Item color="red">
+                    <p>2018-10-16</p>
+                    <p>主题正在征集中</p>
+                    <p>@王建荣</p>
+                </Timeline.Item>
+                {/*<Timeline.Item dot={<Icon type="clock-circle-o" style={{fontSize: '16px'}}/>}>*/}
+                    {/*<p>时间待定</p>*/}
+                    {/*<p>主题正在征集中</p>*/}
+                {/*</Timeline.Item>*/}
             </Timeline>
             <Calendar/>
         </div>
